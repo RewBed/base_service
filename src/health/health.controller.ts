6b31a3ff7c20826738bb8d7b2eadc54dc8b091af
Rewbed <rewbed@gmail.com>
2026-02-07 14:44:01 +0300

    + pagination dto

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { PrismaService } from '../core/database/prisma.service';
 import { HealthDto } from './dto/health.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { HealthPaginationDto } from './dto/health.pagination.dto';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('health')
 export class HealthController {
@@ -24,6 +25,20 @@ export class HealthController {
                 { status: 'error', message: 'Database not ready' },
                 HttpStatus.SERVICE_UNAVAILABLE,
             );
+        }
+    }
+
+    @ApiOkResponse({ type: HealthPaginationDto })
+    @Get()
+    async index(): Promise<HealthPaginationDto> {
+        return {
+            items: [],
+            meta: {
+                total: 1,
+                page: 1,
+                limit: 1,
+                totalPage: 1
+            }
         }
     }
 
